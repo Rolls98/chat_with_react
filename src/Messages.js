1cac7be0bbef4ba6f398464c4560f218c88dc969
rolls98 <toureb16@gmail.com>
2020-08-25 17:31:43 +0000

    sur la voix scroll infinite

--- src/Messages.js
+++ src/Messages.js
@@ -1,48 +1,77 @@
-import React,{useRef,useEffect} from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Message from "./Message";
 import Preview from "./PreviewImg"
 
 import moment from "moment";
 
-export default function Messages({ chats, _id,change }) {
+export default function Messages({ chats, _id, change }) {
   const messagesEndRef = useRef(null);
+  let [allchats, setAllchat] = useState([]);
   const divRef = useRef(null);
   let date = null
   let show = true;
-  
+
+  useEffect(() => {
+    setAllchat(() => chats)
+  }, [chats])
+  const handleScroll = () => {
+    if (divRef.current.scrollTop <= 0) {
+
+      setAllchat((chat) => {
+        let nsChat = [...chat];
+        if (nsChat.length > 0) {
+          console.log("pourquoi ???")
+          nsChat[0].messages.unshift({
+            content: "nouveau message",
+            date: "2020-07-26T14:41:39.817Z",
+            existFile: false,
+            fileName: "",
+            filePath: "",
+            see: false,
+            send_by: "5f1ee9f19fafd76a51cb8a9a"
+          })
+          return nsChat
+        }
+
+      })
+
+      console.log(allchats);
+    }
+  }
+
   const scrollToBottom = () => {
     messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
   };
-  
+
   useEffect(scrollToBottom, [chats]);
-  
+
   return (
-    <div className="messages" ref={divRef}>
+    <div className="messages" ref={divRef} onScroll={handleScroll}>
       <ul >
-        {chats.length > 0 &&
-          chats[0].messages.map((m,index) =>{
-              if(date !== formatDate(m.date)){
-                date = formatDate(m.date);
-                show = true;
-              }else{
-                show = false;
-              }
+        {allchats.length > 0 &&
+          allchats[0].messages.map((m, index) => {
+            if (date !== formatDate(m.date)) {
+              date = formatDate(m.date);
+              show = true;
+            } else {
+              show = false;
+            }
             return (
               <React.Fragment key={index}>
-              {show === true && <li className="date" >{formatDate(m.date)}</li>}
-              <Message
-              msg = {m}
-              content={m.content}
-              key={m.date}
-              class={m.send_by === _id ? "replies" : "sent"}
-              _id={_id}
-            />
-            </React.Fragment>
+                {show === true && <li className="date" >{formatDate(m.date)}</li>}
+                <Message
+                  msg={m}
+                  content={m.content}
+                  key={m.date}
+                  class={m.send_by === _id ? "replies" : "sent"}
+                  _id={_id}
+                />
+              </React.Fragment>
             )
-        })}
+          })}
       </ul>
       <Preview change={change} />
-      <div ref={messagesEndRef}/>
+      <div ref={messagesEndRef} />
     </div>
   );
 }
